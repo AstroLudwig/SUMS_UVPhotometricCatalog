bf180a6f0cc33c3a441661e0ad88536af137de34
Bethany <bethany.a.ludwig@gmail.com>
2025-02-16 22:45:59 +0100

    fixed how magnitudes were being calculated and reran candidate selection. Included using combined errors instead of just mag error. This factors in the std between measurements.

--- 2_CompileCatalog/4_get_blue_sources.py
+++ 2_CompileCatalog/4_get_blue_sources.py
@@ -1,18 +1,19 @@
-import pandas as pd 
-import matplotlib.pyplot as plt
-import numpy as np 
-from scipy.integrate import simps
-import time
 # Create a version of the catalog with extinction and AB mag accounted for.
 # Corrects the photometry for extinction and switches it into AB magnitudes. 
-# At that point it also goes through and figures out if the photometry is 
-# bluewards of the ZAMS in nine different UV optical CMDs.
+# Check if the photometry is bluewards of the ZAMS in nine different UV optical CMDs.
+
+import pandas as pd 
+import numpy as np 
+import time
 
 # Choose which galaxy to work on: 
-galaxy = "smc"
+galaxy = "lmc"
+# Set paths based on machine 
 data_dir = "/home/bethany/Projects/0_Data/"
 step4_dir = data_dir + "0_SUMS_Catalogs/CompleteCatalog/Step4/"
 step5_dir = data_dir + "0_SUMS_Catalogs/CompleteCatalog/Step5/"
+# step4_dir = "/Volumes/Untitled/0_Data/SUMS_Catalogs/Step4/"
+# step5_dir = "/Volumes/Untitled/0_Data/SUMS_Catalogs/Step5/"
 path = step4_dir + f'{galaxy}_photometry.csv'
 save = step5_dir + f'{galaxy}_colors.csv'
 
@@ -34,6 +35,14 @@ def AbsoluteToApparent(AbsoluteMag,distance):
 def Quad(X,Y):
 	return np.sqrt(X**2+Y**2) 
 
+# Combine Mag Errors 
+def CombinedErrors(mag_err,std):
+	# Calculate everything in quadrature
+	combined_errors = Quad(mag_err,std)
+	# Replace anything that was nan (due to no std) with just the error
+	combined_errors[np.isnan(combined_errors)] = mag_err[np.isnan(combined_errors)]
+	return combined_errors
+
 # Function to figure out bluewards vs. not: 
 # Ex: Zams_blue = 'uvm2', zams_red = 'v' for colors 'uvm2 - v'
 def AssessColor(data_x,data_y,data_x_err,zams_blue,zams_red):
@@ -42,36 +51,42 @@ def AssessColor(data_x,data_y,data_x_err,zams_blue,zams_red):
 	x_zams = np.interp(data_y,np.flip(curve_y,0),np.flip(curve_x,0))
 
 	# Check if it is blue or not
-	colors,overlap = [],[]
+	colors = []
 	for i,x in enumerate(data_x):
-		# If left of zams than blue
-		if x < x_zams[i]:
+		# If left of zams than consider it overlap 
+		# unless we prove that it is blue within errors
+		if x < (x_zams[i]):
+			color = 'overlap'
+
+		# If truly left of zams even within errors
+		# than blue
+		if x < (x_zams[i]-data_x_err[i]):
 			color = 'blue'
 
+		# If right of zams than consider it overlap
+		# unless we prove that it is red within errors
+		if x > x_zams[i]:
+			color = 'overlap'
+
 		# If right of zams than red
-		elif x > x_zams[i]:
+		if x > x_zams[i] + data_x_err[i]:
 			color = 'red'
 
-		# If color is off in narnia 
-		elif x < -90:
+		# If color is off in narnia or a NaN
+		if x < -90 or np.isnan(x):
 			color = 'false'
-
-		# If color is a NAN
-		elif np.isnan(x):
-			color = 'false'
-			
-		# Should anything else happen, don't think it will
-		else:
-			color = 'oops'
 
 		colors.append(color)
+	return colors
 
-		# If near zams within error than overlap 
-		if (x > x_zams[i] - data_x_err[i]) and (x < x_zams[i] + data_x_err[i]):
-			overlap.append('yes')
-		else:
-			overlap.append('no')
-	return colors,overlap
+def DistanceToZams(data_x,data_y,zams_blue,zams_red):
+	curve_x = np.array(zams_blue) - np.array(zams_red)
+	curve_y = np.array(zams_blue)
+	x_zams = np.interp(data_y,np.flip(curve_y,0),np.flip(curve_x,0))
+	distance = data_x - x_zams
+	# If distance is an extreme value make it nan 
+	distance[np.abs(distance) > 20] = np.nan
+	return distance
 
 ###############
 # M o d e l s #
@@ -82,8 +97,8 @@ if galaxy == 'smc':
 	distance = smc_distance
 	# Goetberg and MIST Models
 	models = {
-		"zams_ab":   pd.read_csv('C:/Projects/0_Data/Models/ZAMS/ZAMS_Z0.002_ABmag.txt',sep=r'\s+',comment='#'),
-		"shs_ab":	 pd.read_csv('C:/Projects/0_Data/Models/SHS/stripped_stars_Z0.002_ABmag.txt',sep=r'\s+',comment='#')		
+		"zams_ab":   pd.read_csv(data_dir+'1_Models/ZAMS/ZAMS_Z0.002_ABmag.txt',sep=r'\s+',comment='#'),
+		"shs_ab":	 pd.read_csv(data_dir+'1_Models/Stripped_Stars/stripped_stars_Z0.002_ABmag.txt',sep=r'\s+',comment='#')		
 	}
 
 # LMC Models
@@ -91,8 +106,8 @@ if galaxy == 'lmc':
 	distance = lmc_distance
 	# Goetberg and MIST Models
 	models = {
-		"zams_ab":   pd.read_csv('C:/Projects/0_Data/Models/ZAMS/ZAMS_Z0.006_ABmag.txt',sep=r'\s+',comment='#'),
-		"shs_ab":	 pd.read_csv('C:/Projects/0_Data/Models/SHS/stripped_stars_Z0.006_ABmag.txt',sep=r'\s+',comment='#')
+		"zams_ab":   pd.read_csv(data_dir+'1_Models/ZAMS/ZAMS_Z0.006_ABmag.txt',sep=r'\s+',comment='#'),
+		"shs_ab":	 pd.read_csv(data_dir+'1_Models/Stripped_Stars/stripped_stars_Z0.006_ABmag.txt',sep=r'\s+',comment='#')
 	}
 
 # Convert to apparent mags
@@ -120,7 +135,7 @@ df = pd.read_csv(path)
 df.columns = [col.replace('_mag','') for col in df.columns]
 
 # Extinction and Vega to AB
-ref = pd.read_csv('C:/Projects/0_Data/SUMS_Catalogs/ExtinctionAndVega2AB.csv')
+ref = pd.read_csv(f"{data_dir}/0_SUMS_Catalogs/Reference/ExtinctionAndVega2AB.csv")
 
 # Replace null optical with NaN 
 for uvfilter,error in zip(['U','B','V','I'],['e_U','e_B','e_V','e_I']):
@@ -165,54 +180,66 @@ colors = {
 	'uvm2 - i' : df['uvm2_dered'] - df['I_dered']
 }
 
-color_errs = {
-	'uvw2 - b' : Quad(df['uvw2_err'] , df['e_B']),
-	'uvw2 - v' : Quad(df['uvw2_err'] , df['e_V']),
-	'uvw2 - i' : Quad(df['uvw2_err'] , df['e_I']),
-	'uvw1 - b' : Quad(df['uvw1_err'] , df['e_B']),
-	'uvw1 - v' : Quad(df['uvw1_err'] , df['e_V']),
-	'uvw1 - i' : Quad(df['uvw1_err'] , df['e_I']),
-	'uvm2 - b' : Quad(df['uvm2_err'] , df['e_B']),
-	'uvm2 - v' : Quad(df['uvm2_err'] , df['e_V']),
-	'uvm2 - i' : Quad(df['uvm2_err'] , df['e_I'])
+# Combined errors. Quad if both error and std are present otherwise just the error 
+combined_errs = {
+	'uvw2_err' : CombinedErrors(df['uvw2_err'], df['uvw2_std']),
+	'uvw1_err' : CombinedErrors(df['uvw1_err'], df['uvw1_std']),
+	'uvm2_err' : CombinedErrors(df['uvm2_err'], df['uvm2_std']),
 }
+
+color_errs = {
+	'uvw2 - b' : Quad(combined_errs['uvw2_err'] , df['e_B']),
+	'uvw2 - v' : Quad(combined_errs['uvw2_err'] , df['e_V']),
+	'uvw2 - i' : Quad(combined_errs['uvw2_err'] , df['e_I']),
+	'uvw1 - b' : Quad(combined_errs['uvw1_err'] , df['e_B']),
+	'uvw1 - v' : Quad(combined_errs['uvw1_err'] , df['e_V']),
+	'uvw1 - i' : Quad(combined_errs['uvw1_err'] , df['e_I']),
+	'uvm2 - b' : Quad(combined_errs['uvm2_err'] , df['e_B']),
+	'uvm2 - v' : Quad(combined_errs['uvm2_err'] , df['e_V']),
+	'uvm2 - i' : Quad(combined_errs['uvm2_err'] , df['e_I'])
+}
+
 
 
 ##############################
 # Calculate if blue of zams: #
 ##############################
 
-color_labels = {                   # x             # y                   # x_err       # zams_blue  # zams_red
-	'uvw2 - b' : AssessColor(colors['uvw2 - b'],df['uvw2_dered'],color_errs['uvw2 - b'],zams['uvw2'],zams['b'])[0],
-	'uvw2 - v' : AssessColor(colors['uvw2 - v'],df['uvw2_dered'],color_errs['uvw2 - v'],zams['uvw2'],zams['v'])[0],
-	'uvw2 - i' : AssessColor(colors['uvw2 - i'],df['uvw2_dered'],color_errs['uvw2 - i'],zams['uvw2'],zams['i'])[0],
-	'uvw1 - b' : AssessColor(colors['uvw1 - b'],df['uvw1_dered'],color_errs['uvw1 - b'],zams['uvw1'],zams['b'])[0],
-	'uvw1 - v' : AssessColor(colors['uvw1 - v'],df['uvw1_dered'],color_errs['uvw1 - v'],zams['uvw1'],zams['v'])[0],
-	'uvw1 - i' : AssessColor(colors['uvw1 - i'],df['uvw1_dered'],color_errs['uvw1 - i'],zams['uvw1'],zams['i'])[0],
-	'uvm2 - b' : AssessColor(colors['uvm2 - b'],df['uvm2_dered'],color_errs['uvm2 - b'],zams['uvm2'],zams['b'])[0],
-	'uvm2 - v' : AssessColor(colors['uvm2 - v'],df['uvm2_dered'],color_errs['uvm2 - v'],zams['uvm2'],zams['v'])[0],
-	'uvm2 - i' : AssessColor(colors['uvm2 - i'],df['uvm2_dered'],color_errs['uvm2 - i'],zams['uvm2'],zams['i'])[0],
+color_labels = {              # x             # y                # x_err                # zams_blue  # zams_red
+	'uvw2 - b' : AssessColor(colors['uvw2 - b'],df['uvw2_dered'],color_errs['uvw2 - b'],zams['uvw2'],zams['b']),
+	'uvw2 - v' : AssessColor(colors['uvw2 - v'],df['uvw2_dered'],color_errs['uvw2 - v'],zams['uvw2'],zams['v']),
+	'uvw2 - i' : AssessColor(colors['uvw2 - i'],df['uvw2_dered'],color_errs['uvw2 - i'],zams['uvw2'],zams['i']),
+	'uvw1 - b' : AssessColor(colors['uvw1 - b'],df['uvw1_dered'],color_errs['uvw1 - b'],zams['uvw1'],zams['b']),
+	'uvw1 - v' : AssessColor(colors['uvw1 - v'],df['uvw1_dered'],color_errs['uvw1 - v'],zams['uvw1'],zams['v']),
+	'uvw1 - i' : AssessColor(colors['uvw1 - i'],df['uvw1_dered'],color_errs['uvw1 - i'],zams['uvw1'],zams['i']),
+	'uvm2 - b' : AssessColor(colors['uvm2 - b'],df['uvm2_dered'],color_errs['uvm2 - b'],zams['uvm2'],zams['b']),
+	'uvm2 - v' : AssessColor(colors['uvm2 - v'],df['uvm2_dered'],color_errs['uvm2 - v'],zams['uvm2'],zams['v']),
+	'uvm2 - i' : AssessColor(colors['uvm2 - i'],df['uvm2_dered'],color_errs['uvm2 - i'],zams['uvm2'],zams['i']),
 
-	'uvw2 - b overlap' : AssessColor(colors['uvw2 - b'],df['uvw2_dered'],color_errs['uvw2 - b'],zams['uvw2'],zams['b'])[1],
-	'uvw2 - v overlap' : AssessColor(colors['uvw2 - v'],df['uvw2_dered'],color_errs['uvw2 - v'],zams['uvw2'],zams['v'])[1],
-	'uvw2 - i overlap' : AssessColor(colors['uvw2 - i'],df['uvw2_dered'],color_errs['uvw2 - i'],zams['uvw2'],zams['i'])[1],
-	'uvw1 - b overlap' : AssessColor(colors['uvw1 - b'],df['uvw1_dered'],color_errs['uvw1 - b'],zams['uvw1'],zams['b'])[1],
-	'uvw1 - v overlap' : AssessColor(colors['uvw1 - v'],df['uvw1_dered'],color_errs['uvw1 - v'],zams['uvw1'],zams['v'])[1],
-	'uvw1 - i overlap' : AssessColor(colors['uvw1 - i'],df['uvw1_dered'],color_errs['uvw1 - i'],zams['uvw1'],zams['i'])[1],
-	'uvm2 - b overlap' : AssessColor(colors['uvm2 - b'],df['uvm2_dered'],color_errs['uvm2 - b'],zams['uvm2'],zams['b'])[1],
-	'uvm2 - v overlap' : AssessColor(colors['uvm2 - v'],df['uvm2_dered'],color_errs['uvm2 - v'],zams['uvm2'],zams['v'])[1],
-	'uvm2 - i overlap' : AssessColor(colors['uvm2 - i'],df['uvm2_dered'],color_errs['uvm2 - i'],zams['uvm2'],zams['i'])[1]
+	'uvw2 - b distance' : DistanceToZams(colors['uvw2 - b'],df['uvw2_dered'],zams['uvw2'],zams['b']),
+	'uvw2 - v distance' : DistanceToZams(colors['uvw2 - v'],df['uvw2_dered'],zams['uvw2'],zams['v']),
+	'uvw2 - i distance' : DistanceToZams(colors['uvw2 - i'],df['uvw2_dered'],zams['uvw2'],zams['i']),
+	'uvw1 - b distance' : DistanceToZams(colors['uvw1 - b'],df['uvw1_dered'],zams['uvw1'],zams['b']),
+	'uvw1 - v distance' : DistanceToZams(colors['uvw1 - v'],df['uvw1_dered'],zams['uvw1'],zams['v']),
+	'uvw1 - i distance' : DistanceToZams(colors['uvw1 - i'],df['uvw1_dered'],zams['uvw1'],zams['i']),
+	'uvm2 - b distance' : DistanceToZams(colors['uvm2 - b'],df['uvm2_dered'],zams['uvm2'],zams['b']),
+	'uvm2 - v distance' : DistanceToZams(colors['uvm2 - v'],df['uvm2_dered'],zams['uvm2'],zams['v']),
+	'uvm2 - i distance' : DistanceToZams(colors['uvm2 - i'],df['uvm2_dered'],zams['uvm2'],zams['i'])
 
 }
+
 print(f'{galaxy} colors calculated')
 
 ########################
 # Append dictionaries: #
 ########################
 
+# Save Colors and Distance to the ZAMS 
 for key in color_labels.keys():
-
 	df[key] = color_labels[key]
+
+for key in color_errs.keys():
+    df[f'{key} err'] = color_errs[key]
 
 #########
 # Save: #
